98328b4eea888d95d774c787324f81fc45f8db94
Amruth L P <amruthgowd91200@gmail.com>
2023-11-17 09:03:09 +0530

    404

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "../CSS/PageNotFound.css";
 
 const PageNotFound = () => {
   return (
-    <div>
-      Page Not Found
+    <div className="pnbody">
+      <h1 className="h1">404 Error Page </h1>
+      <p class="zoom-area">
+        You’re either misspelling the URL <br /> or requesting a page that's no
+        longer here.{" "}
+      </p>
+      <section class="error-container">
+        <span class="four">
+          <span class="screen-reader-text">4</span>
+        </span>
+        <span class="zero">
+          <span class="screen-reader-text">0</span>
+        </span>
+        <span class="four">
+          <span class="screen-reader-text">4</span>
+        </span>
+      </section>
+      <div class="link-container">
+        <a
+          target="_blank"
+          href="https://www.silocreativo.com/en/creative-examples-404-error-css/"
+          class="more-link"
+        >
+          Visit the original article
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
